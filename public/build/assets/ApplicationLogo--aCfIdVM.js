c0a4efbc899c2438443c576dbb7120e9491c2ef5
Wybie <lyrncofyn@gmail.com>
2024-07-11 00:20:47 +0800

    build for production

--- public/build/assets/ApplicationLogo--aCfIdVM.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-DyCwzmlB.js";function r(o){return t.jsx("img",{...o,src:"/image/logo.png",alt:"logo"})}export{r as A};
---
+++ public/build/assets/ApplicationLogo-B0gi7Rf8.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-DOf8Fck-.js";function r(o){return t.jsx("img",{...o,src:"/image/logo.png",alt:"logo"})}export{r as A};
