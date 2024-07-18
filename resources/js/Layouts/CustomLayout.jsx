e11ec3534f18af61e3e5763482312156ba4d2f8d
wybie <lyrncofyn@gmail.com>
2024-07-19 07:57:31 +0800

    adjust header padding

--- resources/js/Layouts/CustomLayout.jsx
+++ resources/js/Layouts/CustomLayout.jsx
@@ -72,7 +72,7 @@ export default function CustomLayout({ header, links, children }) {
             {header && (
                 <ScrollRightAnimation>
                     <header className="bg-white shadow">
-                        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{header}</div>
+                        <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">{header}</div>
                     </header>
                 </ScrollRightAnimation>
             )}
